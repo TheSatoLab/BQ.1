c226b86a5ecf62e8221f89d885cc3889b39e43a1
ijampei <jampei0513@yahoo.co.jp>
2022-12-16 16:23:27 +0900

    make README

--- Phylogenetic_analysis/Modeling_viral_fitness_evolution/README.md
+++ Phylogenetic_analysis/Modeling_viral_fitness_evolution/README.md
@@ -3,5 +3,26 @@
 ## Summary
 Since our model simply represents the viral lineage growth parameter ($\beta_l$) as the linear combination of the effects of S substitutions, the model can predict the total effect of a set of substations on relative Re. Using this property of the model, we predicted the ancestral relative viral fitness for each node in the BA.5 tree. We calculated these values as the sum of the posterior means of the effects of substitutions of interest. To reconstruct the ancestral relative viral fitness of each node of the BA.5 tree, we first reconstructed the ancestral state of the S substitution profile in each node of the tree using a parsimony method, implemented by the castor package. Subsequently, we predicted the relative viral fitness for each node according to the reconstructed ancestral mutation profile for the node.
 
+## Note
+Please download the GISAID Metadata from the sction "Download packages" (https://gisaid.org/) and save it in this directory.
 
 
+## Usage
+```bash
+
+cd <THIS DIRECTORY>
+
+#extract mutation info
+
+python3 script/summarize_mut_info.ver2.py \
+  metadata.txt \
+  metadata.mut_long.txt
+
+#predict viral fitness evolution
+
+R --vanilla --slave --args \
+  metadata.txt \
+  metadata.mut_long.txt \
+  < script/predict_viral_fitness_evolution.R
+
+```
--- Phylogenetic_analysis/Modeling_viral_fitness_evolution/script/predict_viral_fitness_evolution.R
+++ Phylogenetic_analysis/Modeling_viral_fitness_evolution/script/predict_viral_fitness_evolution.R
@@ -1,84 +1,74 @@
 #!/usr/bin/env R
 
 library(tidyverse)
-library("ape")
-library("ggplot2")
-library("ggtree")
-library(castor)
+library(ggplot2)
 library(scales)
 library(data.table)
-library(cmdstanr)
-library(tictoc)
-library(ggplot2)
 library(patchwork)
 library(RColorBrewer)
-library(ComplexHeatmap)
-library(circlize)
-library(igraph)
-library(makedummies)
 library(ggnewscale)
+library(ape)
+library(ggtree)
+library(castor)
 library(phangorn)
-library(viridis)
 
 
 args = commandArgs(trailingOnly=T)
 
-##########args##########
 
-transition.node.info.name <- '/media/jampei/C6700DA7700D9F75/data/Sato_analysis/convergent_evolution/tree/Omicron_trees.parsimony.transition_branch_info.ba5.txt'
+#input
+
+transition.node.info.name <- '../Ancestral_reconstruction/output/transition_branch.BA.5.txt'
 transition.node.info <- read.table(transition.node.info.name,header=T)
 
-mut.gain.info.name <- '/media/jampei/C6700DA7700D9F75/data/Sato_analysis/convergent_evolution/mutation_regression/2022_11_07/test_2022_11_07.3.method3.growth_gain.txt'
+mut.gain.info.name <- '../../Epidemic_dynamics_modeling_analysis/output/mutation_effect.txt'
 mut.gain.info <- read.table(mut.gain.info.name,header=T)
 
-mut.info.name <- '/media/jampei/C6700DA7700D9F75/data/Sato_analysis/GISAID_data/2022_10_15/metadata.mut_long.Spike.tsv'
-mut.info <- fread(mut.info.name,header=T,sep="\t",check.names=T)
-mut.info <- mut.info %>% mutate(mut = gsub("Spike_","",mut), mut.mod = gsub("[A-Z]$","",mut))
-
-ref.hap.info.name <- '/media/jampei/C6700DA7700D9F75/data/Sato_analysis/convergent_evolution/mutation_regression/2022_11_07/test_2022_11_07.method3.ref_hap.txt'
+ref.hap.info.name <- 'script/reference.haplotype_info.txt'
 ref.hap.info <- read.table(ref.hap.info.name,header=T)
-ref.hap.info <- ref.hap.info %>% mutate(mut = gsub("Spike_","",mut))
 
+tip.df.BQ.1_related.name <- "seq.BQ.1_related.txt"
+tip.df.BQ.1_related <- read.table(tip.df.BQ.1_related.name,header=T)
 
-lineage.interest <- "ba5"
-
-tree.name <- '/media/jampei/C6700DA7700D9F75/data/Sato_analysis/convergent_evolution/tree/nhx_tree/ba5.filt_annot.nwk'
+tree.name <- '../Trees/BA.5.nwk'
 tree <- read.tree(tree.name)
 
 
+metadata.name <- args[1]
+metadata <- fread(metadata.name,header=T,sep="\t",check.names=T)
+
+mut.info.name <- args[2]
+mut.info <- fread(mut.info.name,header=T,sep="\t",check.names=T)
+
+
+#preprocessing
 tip.df <- data.frame(tip_Id = 1:length(tree$tip), Id = tree$tip)
-
-
 tree.info.df <- ggtree(tree)$data
 
-
-
-metadata.name <- '/media/jampei/C6700DA7700D9F75/data/Sato_analysis/GISAID_data/2022_10_15/metadata.tsv'
-metadata <- fread(metadata.name,header=T,sep="\t",check.names=T)
-metadata <- metadata %>% filter(Accession.ID %in% as.character(tip.df$Id))
-
-max.date <- max(as.Date(metadata$Collection.date))
-
+mut.info <- mut.info %>% mutate(mut = gsub("Spike_","",mut), mut.mod = gsub("[A-Z]$","",mut))
 mut.info.interest <- mut.info %>% filter(Id %in% as.character(tip.df$Id))
+
+ref.hap.info <- ref.hap.info %>% mutate(mut = gsub("Spike_","",mut))
+
+metadata <- metadata %>% filter(Accession.ID %in% as.character(tip.df$Id))
+max.date <- max(as.Date(metadata$Collection.date))
 
 
 mut.group.df <- data.frame()
 
 for(i in 1:nrow(mut.gain.info)){
 
-mut.group <- mut.gain.info$mut[i]
-mut.v <- strsplit(mut.group, ":")[[1]]
+  mut.group <- mut.gain.info$mut[i]
+  mut.v <- strsplit(mut.group, ":")[[1]]
 
-temp.df <- data.frame(mut.group = mut.group, mut = mut.v)
+  temp.df <- data.frame(mut.group = mut.group, mut = mut.v)
 
-
-mut.group.df <- rbind(mut.group.df,temp.df)
+  mut.group.df <- rbind(mut.group.df,temp.df)
 
 }
 
 
 mut.info.merged <- mut.info.interest %>% inner_join(mut.group.df,by="mut")
-
 mut.info.merged <- mut.info.merged %>% distinct(mut.group,Id,.keep_all=T)
 
 mut.info.mat <- mut.info.merged %>% mutate(value = 1) %>% select(-mut,-mut.mod) %>% spread(key=mut.group,value=value)
@@ -93,49 +83,28 @@ mut.info.mat <- mut.info.mat[as.character(tip.df$Id),]
 
 mut.gain.info.filtered <- mut.gain.info %>% filter(mut %in% colnames(mut.info.mat))
 
-#mut.gain.info.filtered <- mut.gain.info.filtered %>% inner_join(ref.hap.info,by="mut")
-#mut.gain.info.filtered <- mut.gain.info.filtered %>% mutate(mean.log = log(mean),mean.log.directed = ifelse(hap == 0, as.numeric(mean.log),-as.numeric(mean.log)))
-
-
 mut.info.mat <- mut.info.mat %>% select(all_of(mut.gain.info.filtered$mut))
 
+#the ancestral state reconstruction of S substitutions
 sum.ancestral.l <- list()
-
 
 for(i in 1:ncol(mut.info.mat)){
 
-mut.group.interest <- colnames(mut.info.mat)[i]
+  mut.group.interest <- colnames(mut.info.mat)[i]
 
-state.v <- as.numeric(mut.info.mat[,i]) + 1
-
-
-fit.asr <- asr_max_parsimony(tree, state.v, Nstates=NULL, 
-             transition_costs="all_equal",
-             edge_exponent=0, weight_by_scenarios=TRUE,
-             check_input=TRUE)
+  state.v <- as.numeric(mut.info.mat[,i]) + 1
 
 
-#fit.asr <- asr_mk_model(tree, state.v, 
-#              Nstates = NULL, 
-#              tip_priors = NULL, 
-#              rate_model = "ER", 
-#              transition_matrix = NULL, 
-#              include_ancestral_likelihoods = TRUE, 
-#              reroot = TRUE,
-#              root_prior = "empirical", 
-#              Ntrials = 1, 
-#              optim_algorithm = "nlminb",
-#              optim_max_iterations = 200,
-#              optim_rel_tol = 1e-8,
-#              store_exponentials = TRUE,
-#              check_input =TRUE, 
-#              Nthreads = 1)
+  fit.asr <- asr_max_parsimony(tree, state.v, Nstates=NULL, 
+               transition_costs="all_equal",
+               edge_exponent=0, weight_by_scenarios=TRUE,
+               check_input=TRUE)
 
-ancstats <- as.data.frame(fit.asr$ancestral_likelihoods)
-colnames(ancstats) <- c('absence','presence')
-ancstats$node <- 1:tree$Nnode + Ntip(tree)
+  ancstats <- as.data.frame(fit.asr$ancestral_likelihoods)
+  colnames(ancstats) <- c('absence','presence')
+  ancstats$node <- 1:tree$Nnode + Ntip(tree)
 
-sum.ancestral.l[[mut.group.interest]] <- c(as.numeric(mut.info.mat[,i]),ancstats$presence)
+  sum.ancestral.l[[mut.group.interest]] <- c(as.numeric(mut.info.mat[,i]),ancstats$presence)
 
 }
 
@@ -144,6 +113,8 @@ sum.ancestral.df <- as.data.frame(t(do.call(Unzip, sum.ancestral.l)))
 colnames(sum.ancestral.df) <- colnames(mut.info.mat)
 rownames(sum.ancestral.df) <- 1:nrow(sum.ancestral.df)
 
+
+#predict ancestral viral fitness
 mut.gain.info.filtered <- mut.gain.info %>% filter(mut %in% colnames(sum.ancestral.df))
 
 sum.ancestral.df <- sum.ancestral.df %>% select(all_of(mut.gain.info.filtered$mut))
@@ -158,15 +129,13 @@ sum.ancestral.df <- t(t(sum.ancestral.df) - ref.hap.info.filtered$hap)
 mut.effect.df <- t(t(sum.ancestral.df) * log(mut.gain.info.filtered$mean)) %>% as.data.frame()
 
 
-
 predict.df.Re <- data.frame(node = tree.info.df$node, Re = exp(apply(mut.effect.df,1,sum)))
-
-
 
 mut.info.spread <- mut.info.interest %>% select(Id,mut.mod) %>% mutate(value = 1) %>% spread(key=mut.mod,value =value)
 
 
 
+#plot
 tip.df.merged <- tip.df %>% left_join(mut.info.spread,by="Id")
 tip.df.merged[is.na(tip.df.merged)] <- 0
 
@@ -177,14 +146,7 @@ names(color.mut.v) <- c("R346","K444","L452","N460","F486")
 
 print("tree")
 
-col.v <- viridis(100)
-
-
-
 n.seq <- nrow(tip.df)
-
-
-
 
 node.df <- transition.node.info %>% select(node,mut)
 
@@ -194,8 +156,7 @@ rownames(plot.df) <- tip.df.merged$Id
 plot.df <- ifelse(plot.df == 1,"+","-")
 plot.df <- plot.df[,c("R346","K444","L452","N460","F486")]
 
-#plot.df <- plot.df[tip.df2$Id,]
-
+#BA.5 tree
 g <- ggtree(tree,size=0.5,mrsd = max.date) %>% flip(5502,5531)
 g <- g %<+% predict.df.Re + aes(color=Re) 
 g <- g + scale_colour_gradientn(colors=c("gray70","red","orange","yellow"),breaks=c(1.25,1.4,1.55),limits=c(1.25,1.55),oob=squish) 
@@ -210,42 +171,29 @@ g1 <- gheatmap(g, plot.df, offset = 0.2, width = 0.3,
                colnames_position = "top",
                colnames_offset_y = 0,colnames_angle=90,hjust=0,color=NA)
 g1 <- g1 + scale_fill_manual(values=c('gray90','navy'),breaks=c("-","+"))
-g1 <- g1 + ggtitle(lineage.interest)
+g1 <- g1 + ggtitle("BA.5")
 
 
-pdf.name <- '/media/jampei/C6700DA7700D9F75/data/Sato_analysis/convergent_evolution/tree/BA5_tree.Re.pdf'
+#subtree
+mrca.subtree <- getMRCA(tree, tip.df.BQ.1_related$Id)
+
+mrca.subtree.parent <- Ancestors(tree, mrca.subtree, type = c("parent"))
+
+g2 <- viewClade(g1,node=mrca.subtree.parent)
+
+
+
+#plot output
+pdf.name <- 'output/BA5_tree.Re.pdf'
 pdf(pdf.name,width=8,height=5)
 g1
 dev.off()
 
 
 
-#subtree
-subtree.name <- '/media/jampei/C6700DA7700D9F75/data/Sato_analysis/convergent_evolution/tree/ba5sub4_filt_bq11.nwk'
-subtree <- read.tree(subtree.name)
-
-subtree.tip.df <- data.frame(tip_Id = 1:length(subtree$tip), Id = subtree$tip)
-
-mrca.subtree <- getMRCA(tree, subtree.tip.df$Id)
-
-mrca.subtree.parent <- Ancestors(tree, mrca.subtree, type = c("parent"))
-
-#g1 <- g1 + geom_text(aes(label=node), hjust=-.3,color="black")
-g2 <- viewClade(g1,node=mrca.subtree.parent)
-g2
-
-
-pdf.name <- '/media/jampei/C6700DA7700D9F75/data/Sato_analysis/convergent_evolution/tree/BQ11_tree.Re.pdf'
+pdf.name <- 'output/BQ11_tree.Re.pdf'
 pdf(pdf.name,width=8,height=5)
 g2
 dev.off()
-
-
-
-
-
-
-
-
 
 
